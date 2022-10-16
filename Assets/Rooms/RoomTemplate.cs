674a401659bfdefa713ba56517ad4a9e7c277787
Hudson DeAlmeida <107660641+HudsonND@users.noreply.github.com>
2022-10-16 17:21:14 -0400

    Assets

---
+++ Assets/Rooms/RoomTemplate.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoomTemplate : MonoBehaviour
+{
+    public GameObject[] bottomRooms;
+    public GameObject[] topRooms;
+    public GameObject[] leftRooms;
+    public GameObject[] rightRooms;
+    public GameObject closedRooms;
+}
