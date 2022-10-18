8c20b9850f2d7fed70f888e61ec658fa5839aeac
Hudson DeAlmeida <107660641+HudsonND@users.noreply.github.com>
2022-10-17 23:46:43 -0400

    added wall tag and layer to all rooms

--- Assets/Rooms/Room Scripts/RoomSpawner.cs
+++ Assets/Rooms/Room Scripts/RoomSpawner.cs
@@ -18,7 +18,7 @@ public class RoomSpawner : MonoBehaviour
     {
         Destroy(gameObject, waitTime);
         templates = GameObject.FindGameObjectWithTag("Rooms").GetComponent<RoomTemplate>();
-        Invoke("Spawn", 0.1f);
+        Invoke("Spawn", 0.2f);
     }
     
     void Spawn()
