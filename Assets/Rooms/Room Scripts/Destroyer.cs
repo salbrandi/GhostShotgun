206091dcebc544d53aa6b5ca900477e983658065
Hudson DeAlmeida <107660641+HudsonND@users.noreply.github.com>
2022-10-18 14:13:09 -0400

    Updated Destroy gameObject

--- Assets/Rooms/Room Scripts/Destroyer.cs
+++ Assets/Rooms/Room Scripts/Destroyer.cs
@@ -5,18 +5,11 @@ using UnityEngine;
 public class Destroyer : MonoBehaviour
 {
     public GameObject startPoint;
-    private bool deleted = false;
+
 
     void OnTriggerEnter2D(Collider2D other)
     {
         Destroy(other.gameObject);
-        deleted = true;
-        Invoke("SelfDistruct", 0.4f);
-       
     }
-    void SelfDistruct()
-    {
-        if(deleted = true)
-            Destroy(gameObject);
-    }
+
 }
